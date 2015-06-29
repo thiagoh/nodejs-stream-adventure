89abd04cd931939da2e15f1d811ad3341388463f
Thiago Andrade <thiagoh@gmail.com>
2015-06-29 11:57:36 -0300

    through2

--- stream3.js
+++ stream3.js
@@ -1,1 +1,8 @@
 var through = require('through2');
+
+var stream = through(function(buf, encoding, next) {
+	this.push(buf.toString().toUpperCase());
+	next();
+});
+
+process.stdin.pipe(stream).pipe(process.stdout);
